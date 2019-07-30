45449c6dec23f0b983012927ea3b3afecc96462d
Nacho <nachomattos78@gmail.com>
2019-07-30 21:09:18 +0100

    productosPHP

---
+++ productos.php
@@ -0,0 +1,9 @@
+<?php
+
+$productos = [
+    ["nombre" => "cerveza", "precio" => 4],
+    ["nombre" => "bocadillo", "precio" => 6],
+    ["nombre" => "refresco", "precio" => 2],
+];
+
+echo json_encode($productos);
