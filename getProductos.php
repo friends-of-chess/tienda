a21104367aae1d433ed86f0e79545d04b4e8e14a
Oversv <albertomdv@gmail.com>
2019-08-06 20:47:02 +0100

    Obtener productos a través de su id

---
+++ getProductos.php
@@ -0,0 +1,54 @@
+<?php
+//Constantes para la conexión
+define("SERVER", "localhost");
+define("USER", "root");
+define("PASS", "");
+define("DB_NAME", "tienda");
+
+//Almacena el producto
+$producto=[];
+
+//Conexión BBDD
+$db = new mysqli(SERVER, USER, PASS, DB_NAME);
+
+if($db->connect_error){
+    die("Conexión fallida. ". $db->connect_error."<br>") ;
+}
+
+//Preparación de la consulta
+$stmt = $db->prepare("SELECT nombre, descripcion, precio, imagen, stock FROM productos WHERE id=?");
+$stmt->bind_param('i', $id);
+
+if(!isset($_GET['id'])){    
+    $db->close();
+    echo "No existe ningún ID.<br>";
+}else{
+    $id=$_GET['id'];  
+    $stmt->execute();
+    $stmt->bind_result($nombre, $descripcion, $precio, $imagen, $stock); 
+
+    //Guardar datos
+    while($stmt->fetch()){   
+        $producto =[
+            "nombre" => $nombre,
+            "descripcion" => $descripcion,
+            "precio" => $precio,
+            "imagen" => $imagen,
+            "stock" => $stock
+        ];    
+    }
+    
+    if(count($producto) == 0){
+        echo "El ID: ". $id . " no está asociado a ningún producto.";
+        $stmt->close();
+        $db->close();    
+
+    }else{
+        $stmt->close();
+        $db->close();
+    
+        header("Content-type: application/json");
+        echo json_encode($producto);
+    }
+}
+?>
